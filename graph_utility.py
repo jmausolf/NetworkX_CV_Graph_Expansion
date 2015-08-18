978adf218f2cf1972dbbd6aff22ad0d259cbb59b
jmausolf <jgm346@gmail.com>
2015-08-18 12:08:36 -0500

    Cleaning up graph utility.

--- graph_utility.py
+++ graph_utility.py
@@ -8,6 +8,12 @@
 
 import networkx as nx
 
+
+#____________________________________________________________________#
+#                                                                    #
+## <---------------       Edge List Utility        ---------------> ##
+#____________________________________________________________________#
+
 def edge_list(graphfile, data_option, stat_option, print_write="pass", for_label="no_label", load="graphfile"):
 	
 	if load == "graphfile":
@@ -111,7 +117,13 @@ def edge_list(graphfile, data_option, stat_option, print_write="pass", for_label
 #edge_list("2008_multigraph.graphml", True, "Yes", "pass")
 #edge_list("2008_edges_added_multigraph.graphml", True, "Yes", "write")
 #edge_list("2008_edges_removed_multigraph.graphml", True, "Yes", "write")
+#tedge_list("entity_date_multigraph_fx.graphml", True, "Yes", "print")
 
+
+#____________________________________________________________________#
+#                                                                    #
+## <---------------       Graph Manipulation       ---------------> ##
+#____________________________________________________________________#
 
 def graph_nodes(graphfile, test="no"):
 	
@@ -177,123 +189,13 @@ def graph_nodes(graphfile, test="no"):
 	elif test == "test":
 		edge_list(outfile, True, "Yes", "print")
 
-
-
 #graph_nodes("entity_date_multigraph_fx.graphml")
 
 
-
-def make_filtered_subgraph(graphfile, year, test="no"):
-	"""This function creates a subgraph for a given edge year
-	Has correct nodes, but they lack attributes."""
-
-	_graphfile = str(graphfile)
-	_year = str(year)
-
-	G=nx.read_graphml(graphfile)
-	#SG=nx.MultiGraph( [ (u,v,d) for u,v,d in G.edges(data=True) if _year in d['date'].split(':', 1)[0]])
-	SG=nx.MultiGraph( [ (u,v,d) for u,v,d in G.edges(data=True) if _year in d['date'].split(':', 1)[0]])
-
-	outfile = _year+"_multigraph.graphml"
-	nx.write_graphml(SG, outfile)
-
-	if test == "no":
-		pass
-	elif test == "test":
-		edge_list(outfile, True, "Yes", "print")
-
-#make_filtered_subgraph("entity_date_multigraph.graphml", "1982", "test")
-#make_filtered_subgraph("entity_date_multigraph.graphml", "1996", "test")
-#make_filtered_subgraph("entity_date_multigraph.graphml", "1995", "test")
-#make_filtered_subgraph("entity_date_multigraph.graphml", "1995", "test")
-#make_filtered_subgraph("entity_date_multigraph.graphml", "1972", "test")
-#make_filtered_subgraph("entity_date_multigraph.graphml", "1977", "test")
-#make_filtered_subgraph("entity_date_multigraph.graphml", "1979", "test")
-#make_filtered_subgraph("entity_date_multigraph.graphml", "1982", "test")
-#make_filtered_subgraph("entity_date_multigraph.graphml", "1989", "test")
-#make_filtered_subgraph("entity_date_multigraph.graphml", "2008", "test")
-#make_filtered_subgraph("entity_date_multigraph.graphml", "2009", "test")
-
-
-def make_filtered_subgraph_dev1(graphfile, year, test="no"):
-	"""This function creates a subgraph for a given edge year
-	Gets the nodes attributes added but has nodes without edges in subgraph
-	"""
-	
-	_graphfile = str(graphfile)
-	_year = str(year)
-
-	G=nx.read_graphml(graphfile)
-	for u, d in G.nodes(data=True):
-
-
-		#Creating Name Labels
-		try:
-			name0 = str(d['name']).split('-', 1)[1]
-		except:
-			print "Exception occurred. Check to see if all names follow DEPT_DEPT-NAME_NAME format. These issues are resolved for file: 'entity_date_multigraph_fx.graphml' in the University of Chicago Network."
-			name0 = str(d['name']).split('_', 1)[1].split('_', 1)[0]
-			if len(name0) <=4:
-				name0 = str(d['name']).split('_', 1)[1].split('_', 1)[1]
-			elif len(name0) > 4:
-				name0 = str(d['name']).split('_', 1)[1]
-
-		#Full Name - Cleaned
-		name = name0.split('.')[0].replace('_', ' ')
-
-		#Creating Department Labels and Groups
-		try: 
-			dept0 = str(d['name']).split('-', 1)[0]
-			dept = "("+str(dept0.replace('_', ', '))+")"
-			#print dept
-		
-		except:
-			print "Exception occurred. Check to see if all names follow DEPT_DEPT-NAME_NAME format. These issues are resolved for file: 'entity_date_multigraph_fx.graphml' in the University of Chicago Network."
-
-		#Creating Department Group
-		dg = str(d['name']).split('-')[0].split('_')[0:1]
-		
-		for D in dg:
-
-			#Group Religion Labels into Single Department
-			if "THEO" in D or "JWSC" in D or "HREL" in D or "HCHR" in D or "ISLM" in D or "HIJD" in D or "RETH" in D or "BIBL" in D or "DVPR" in D:
-				dept_group = "THEO"
-
-			#Every Other Department
-			else:
-				dept_group = str(d['name']).split('-')[0].split('_', 1)[0]
-
-		print dept_group
-		G.add_node(u, name=str(d['name']), Label=str(name+' - '+dept_group), Departments=dept, Department_Group=dept_group, Name=name)
-		#G.add_node(u, name=str(d['name']), Label=name, Department_Group=dept_group)
-
-	#Create Subgraph with Edges for Specified Year
-	SG=nx.MultiGraph( [ (u,v,d) for u,v,d in G.edges(data=True) if _year in d['date'].split(':', 1)[0]])
-	
-	#Copy All Nodes and Node Data to Subgraph
-	SG.add_nodes_from(G.nodes(data=True))
-	
-	#Remove Nodes without Edges from Subgraph
-	SG.remove_nodes_from((n for n,d in SG.degree_iter() if d==0))
-
-	outfile = _year+"_test_multigraph.graphml"
-	nx.write_graphml(SG, outfile)
-
-	#Print Options
-	if test == "no":
-		edge_list(outfile, True, "Yes")
-		pass
-	elif test == "test":
-		edge_list(outfile, True, "Yes", "print")
-
-
-#make_filtered_subgraph_dev("entity_date_multigraph_fx.graphml", "1998")
-#make_filtered_subgraph_dev("entity_date_multigraph_fx.graphml", "2009")
-#make_filtered_subgraph_dev("entity_date_multigraph_fx.graphml", "2008")
-#make_filtered_subgraph_dev("EXPANDED_entity_date_multigraph_fx.graphml", "2004")
-#make_filtered_subgraph_dev("EXPANDED_entity_date_multigraph_fx.graphml", "1998", "test")
-
-
+#____________________________________________________________________#
+#                                                                    #
+## <---------------       Subgraph Creation        ---------------> ##
+#____________________________________________________________________#
 
 
 def subgraph(graphfile, year, test="no"):
